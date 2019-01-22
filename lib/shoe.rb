a05b41c08713bed0a99ab46be4a0cc391bdd865e
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-22 19:22:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,18 @@
 class Shoe
   attr_accessor :color, :size , :material
+attr_reader :title , :conditoion 
+
+def initialize(brand)
     @brand = brand
-  end
-    def brand
-      @brand
-    end
+end
+    
+  
+
     def cobble
       puts "Your shoe is as good as new!"
       end
 
       def condition(condition)
         @condition = condition
-        
-      end
+        end
 end
